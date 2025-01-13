b8ea848c3f1b2a50fbb598cab67f2cd727f88c61
akshsgaur <akshitgaur997@gmail.com>
2025-01-13 15:27:12 -0800

    Changes to groupAnagrams

--- GroupedAnagrams.py
+++ GroupedAnagrams.py
@@ -31,17 +31,28 @@ import time
 
 class Solution:
 
-    def GroupedAnagrams(self, strs):
-        res = defaultdict(list) #mapping charCount to the list of Anagrams
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # The res that we will store the key and the value in
+        # and then return the values of the list in
+        res = defaultdict(list)
 
-        for s in strs:
+        #Looping through each string
+        for s in strs: 
+            # Creating a list of 26 characters which we will manipulate each value in the 
+            # list
             count = [0] * 26
+            
+            #Going through each character in the string and then manipulating the 
+            #index to add 1 to that list
+            for c in s: 
+                # ord(c) - ord("a") will give us the index of that character, for
+                # example c will be 2 - 0 
+                count[ord(c) - ord("a")] += 1
 
-            for c in s:
-                count[ord(c)-ord("a")] += 1
-
+            #creating a key of the list count, while the value will be that string
             res[tuple(count)].append(s)
 
+        #Returning the value
         return res.values()
 
 
